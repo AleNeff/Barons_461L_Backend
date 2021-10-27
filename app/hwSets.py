64562140192a8eb533250f5502d501f5d533f333
K5p5 <60909305+K5p5@users.noreply.github.com>
2021-10-26 19:17:04 -0500

    Update hwSets.py

--- app/hwSets.py
+++ app/hwSets.py
@@ -1,17 +1,86 @@
 from pymongo import MongoClient
 #Providing the mongodb atlas url to connect python to mongodb using pymongo
 import ssl
+import json
+from bson import json_util
 #Use Cluster0
 client = MongoClient('mongodb+srv://kpangottil:barons@cluster0.yrjds.mongodb.net/appDB?retryWrites=true&w=majority',ssl_cert_reqs=ssl.CERT_NONE)
 dbname = client["appDB"]
-newCollection = dbname["Hardware Sets"]
-def add_HardwareSet(name):
-    name = {
-    "Capacity": "200",
-    "Availability": "200",
-    "Requests":"0"
+setCollection = dbname["Hardware Sets"]
+class hardwareSet():
+    name: str
+    capacity: int 
+    availability: int
+    requests: int = 0
+    def __init__(self, name):
+        self.name = name
+        self.capacity = 200
+        self.availability = 200
+    def create_sets(self):
+        initialize_HardwareSets(self.capacity)
+def initialize_HardwareSets(capacity):
+    HWSet1 = {
+    "Name": "Hardware Set 1",
+    "Capacity": capacity,
+    "Availability": capacity,
     }
-    newCollection.insert_one(name)
-set_name = "HWSet1"
-add_HardwareSet(set_name)
+    HWSet2 = {
+    "Name": "Hardware Set 2",
+    "Capacity": capacity,
+    "Availability": capacity,
+    }
+    setCollection.insert_one(HWSet1)
+    setCollection.insert_one(HWSet2)
+    return
+
+def add_HardwareSets(name="", capacity=0):
+    set = {
+        "Name": name,
+        "Capacity": capacity,
+        "Availability":capacity
+    }
+    setCollection.insert_one(set)
+    return
+
+def get_HWSet(name):
+    hwSet = setCollection.find_one({"Name": name})
+    hwSet_json = json.dumps(hwSet, indent=4, default=json_util.default)
+    return hwSet_json
+
+def checkout(self,name, count):
+    search = {"Name": name}
+    self.availability -= count
+    new_availability = {"$set" : {'Availability':self.availability}}
+    set = setCollection.update_one(search, new_availability)
+    return
+
+def check_in(self,name, count):
+    search = {"Name": name}
+    self.availability += count
+    new_availability = {"$set" : {'Availability':self.availability}}
+    set = setCollection.update_one(search, new_availability)
+    return
+
+def update_capacity(self, name, count):
+    search = {"Name": name}
+    self.availability = self.capacity - self.availability + count
+    self.capacity = count
+    new_capacity = {"$set":{'Capacity': count}}
+    set = setCollection.update_one(search, new_capacity)
+    new_availability = {"$set" : {'Availability':self.availability}}
+    set = setCollection.update_one(search, new_availability)
+    return
+
+def testing_functions():
+    x = hardwareSet(set)
+    initialize_HardwareSets(200)
+    hw_set = get_HWSet("Hardware Set 1")
+    print(hw_set)
+    update_capacity(x, "Hardware Set 1", 300)
+    checkout(x, "Hardware Set 1", 100)
+    check_in(x, "Hardware Set 1", 100)
+    hw_set = get_HWSet("Hardware Set 1")
+    add_HardwareSets("Test", 400)
+    print(hw_set)
+testing_functions()
 client.close()
