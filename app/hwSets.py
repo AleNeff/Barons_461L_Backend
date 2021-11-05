ccaf5bd9a83ed83c39a4e27e94bbcfd59e2116b3
K5p5 <60909305+K5p5@users.noreply.github.com>
2021-11-05 17:32:16 -0500

    Update hwSets.py

--- app/hwSets.py
+++ app/hwSets.py
@@ -7,17 +7,7 @@ from bson import json_util
 client = MongoClient('mongodb+srv://kpangottil:barons@cluster0.yrjds.mongodb.net/appDB?retryWrites=true&w=majority',ssl_cert_reqs=ssl.CERT_NONE)
 dbname = client["appDB"]
 setCollection = dbname["Hardware Sets"]
-class hardwareSet():
-    name: str
-    capacity: int 
-    availability: int
-    requests: int = 0
-    def __init__(self, name):
-        self.name = name
-        self.capacity = 200
-        self.availability = 200
-    def create_sets(self):
-        initialize_HardwareSets(self.capacity)
+
 def initialize_HardwareSets(capacity):
     HWSet1 = {
     "Name": "Hardware Set 1",
@@ -43,14 +33,10 @@ def add_HardwareSets(name="", capacity=0):
     return
 
 def remove_HWset(name):
-    message = ""
     if setCollection.find_one({"Name":name}) is not None:
         hwSet = setCollection.find_one({"Name": name})
         setCollection.delete_one(hwSet)
-        message = name + " has been successfully deleted"
-    else:
-        message = name + " does not exist"
-    return message
+       
 
 def get_HWSet(name):
     if setCollection.find_one({"Name":name}) is not None:
@@ -58,43 +44,39 @@ def get_HWSet(name):
             hwSet_json = json.dumps(hwSet, indent=4, default=json_util.default)
             return hwSet_json
 
+#returns all hardware sets
 
-def checkout(self,name, count):
-    search = {"Name": name}
-    self.availability -= count
-    new_availability = {"$set" : {'Availability':self.availability}}
-    set = setCollection.update_one(search, new_availability)
-    return
+def get_all_hardwareSets():
+    """
+    Get all projects from MongoDB with username user (str)
+    """
+    hwset_list = setCollection.find()
+    result = []
+    for each_set in hwset_list:
+        result.append(each_set)
+    return result
 
-def check_in(self,name, count):
-    search = {"Name": name}
-    self.availability += count
-    new_availability = {"$set" : {'Availability':self.availability}}
-    set = setCollection.update_one(search, new_availability)
-    return
-
-def update_capacity(self, name, count):
-    search = {"Name": name}
-    self.availability = self.capacity - self.availability + count
-    self.capacity = count
-    new_capacity = {"$set":{'Capacity': count}}
-    set = setCollection.update_one(search, new_capacity)
-    new_availability = {"$set" : {'Availability':self.availability}}
-    set = setCollection.update_one(search, new_availability)
+def update_capacity(name, count):
+    if setCollection.find_one({"Name":name}) is not None:
+        search = setCollection.find_one({"Name":name})
+        new_capacity = {"$set":{'Capacity': count}}
+        setCollection.update_one(search, new_capacity)
+        new_availability = {"$set" : {'Availability':count}}
+        search = setCollection.find_one({"Name":name})
+        setCollection.update_one(search, new_availability)
     return
 
 def testing_functions():
-    x = hardwareSet(set)
-    initialize_HardwareSets(200)
+    #initialize_HardwareSets(200)
     hw_set = get_HWSet("Hardware Set 1")
     print(hw_set)
-    update_capacity(x, "Hardware Set 1", 300)
-    checkout(x, "Hardware Set 1", 100)
-    check_in(x, "Hardware Set 1", 100)
-    hw_set = get_HWSet("Hardware Set")
+    update_capacity("Hardware Set 1", 300)
+    hw_set = get_HWSet("Hardware Set 2")
     add_HardwareSets("Test", 400)
     out = remove_HWset("Test")
-    print(out)
-    print(hw_set)
+   # print(out)
+    #print(hw_set)
+    print(get_all_hardwareSets())
 testing_functions()
 client.close()
+
